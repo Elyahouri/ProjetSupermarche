99d314ff7c3cfcbb3762bc48080c272b9c9b1a0e
Elyahouri <elyahouri52@gmail.com>
2022-04-11 13:54:04 +0200

    Construction des differentes classes

---
+++ src/projetsupermarche/Rayon.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetsupermarche;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author elyah
+ */
+public class Rayon {
+    private int idRayon;
+    private String nomRayon;
+    private ArrayList<Travailler> lesTravailler;
+    
+    public Rayon(int unId,String unNom){
+        idRayon=unId;
+        nomRayon=unNom;
+        lesTravailler=new ArrayList<>();
+    }
+
+    /**
+     * @return the idRayon
+     */
+    public int getIdRayon() {
+        return idRayon;
+    }
+
+    /**
+     * @return the nomRayon
+     */
+    public String getNomRayon() {
+        return nomRayon;
+    }
+
+    /**
+     * @return the lesTravailler
+     */
+    public ArrayList<Travailler> getLesTravailler() {
+        return lesTravailler;
+    }
+    
+    
+}
