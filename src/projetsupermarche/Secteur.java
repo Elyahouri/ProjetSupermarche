f33a5ec9578597c6abff8d934c7d0719704bf4c8
Elyahouri <elyahouri52@gmail.com>
2022-04-11 11:20:00 +0200

    Création du projet supermarche

---
+++ src/projetsupermarche/Secteur.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetsupermarche;
+
+/**
+ *
+ * @author elyah
+ */
+public class Secteur {
+    private int idSecteur;
+    private String nomSecteur;
+    
+    public Secteur(int unId,String unNom){
+        idSecteur=unId;
+        nomSecteur=unNom;
+    
+    }
+    
+}
