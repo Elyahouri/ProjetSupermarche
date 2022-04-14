502734a9606cdba40b53684c0bc36f7e745acf1f
Elyahouri <elyahouri52@gmail.com>
2022-04-14 11:19:27 +0200

    Construction des differentes classes

--- src/SuperMarche/Supermarche.java
+++ src/SuperMarche/Supermarche.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package projetSupermarche;
+package SuperMarche;
 
 /**
  *
  * @author elyah
  */
-public class SuperMarche {
+public class Supermarche {
 
     /**
      * @param args the command line arguments
