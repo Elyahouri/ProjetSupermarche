10aa3a1f66af131991675cd71688507893c97ce9
Elyahouri <elyahouri52@gmail.com>
2022-04-14 11:10:36 +0200

    Construction des differentes classes

---
+++ src/Class/Employe.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+/**
+ *
+ * @author elyah
+ */
+public class Employe {
+    private int idEmploye;
+    private String nomEmploye;
+    
+    public Employe(int unId, String unNom){
+        idEmploye=unId;
+        nomEmploye=unNom;
+    }
+
+    /**
+     * @return the idEmploye
+     */
+    public int getIdEmploye() {
+        return idEmploye;
+    }
+
+    /**
+     * @return the nomEmploye
+     */
+    public String getNomEmploye() {
+        return nomEmploye;
+    }
+    
+}
